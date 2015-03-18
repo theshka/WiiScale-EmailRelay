8cda9d48e42e2931091934793bdb55a35373a356
theshka <t.heshka@gmail.com>
2015-03-18 14:03:27 -0400

    Update relay.php

--- relay.php
+++ relay.php
@@ -1,16 +1,19 @@
 <?php 
 
-/***
+/**
  * WiiScale - Email Relay
  * Use a WiiFit Balance Board as 'Smart Scale' and have it automatically sync with FitBit & Apple Health.
  *
  * Instructions: https://github.com/theshka/WiiScale-EmailRelay
  */
 
-//Settings
-define(FROM_EMAIL, "you@IFTTTEmailChannel.com");
+/**
+ * Settings
+ */
+ define(FROM_EMAIL, "you@IFTTTEmailChannel.com");
+ //No need to edit anything below...
 
-// No need to edit anything below... 
+//Mailer function.
 function sendWeight($mass) 
 {
 	$to = "trigger@recipe.ifttt.com";
@@ -19,15 +22,18 @@ function sendWeight($mass)
 	$headers = "MIME-Version: 1.0" . "\r\n";
 	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 	
+	//use sendmail transport protocol. 
 	mail($to,$subject,$mass,$headers);
 }
 
 //Listen for WiiScale call... 
 if(isset($_SERVER['CONTENT_TYPE']) == "application/x-www-form-urlencoded")
 {
-	$mass = round($_POST['mass'], 4);
-	sendWeight($mass);
+	//send weight (in kg) to IFTTT, rounded to the nearest 1/10000th
+	sendWeight(round($_POST['mass'], 4));
 }
-else {
-    echo 'It works! Congrats. Point your WiiScale URL here!';
+else 
+{
+	//if viewing directly, show a friendly message. 
+	echo 'It works! Congrats. Point your WiiScale URL here!';
 }
